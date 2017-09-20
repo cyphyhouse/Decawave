62e9246db2f1e858c03f3cab53004fd770400085
Yhoas Olivas Hernandez <yholivas@gmail.com>
2017-09-20 20:10:41 +0000

    started working on GPS conversion

--- ROS/cyphyhouse/quadcopter/src/fakegps.cpp
+++ ROS/cyphyhouse/quadcopter/src/fakegps.cpp
@@ -1,13 +1,25 @@
 #include <cstdio>
 #include <iostream>
+#include <cmath>
+
+#include <GeographicLib/Geocentric.hpp>
+#include <GeographicLib/LocalCartesian.hpp>
 
 #include "ros/ros.h"
 #include "geometry_msgs/Point.h"
 #include "ros/package.h"
 
+static const double lat0 = 40 + 7/60.0, lon0 = -88 - 14/60.0; // CSL GPS coords
+
+static GeographicLib::Geocentric earth(GeographicLib::Constants::WGS84_a(), GeographicLib::Constants::WGS84_f());
+static GeographicLib::LocalCartesian proj(lat0, lon0, 0, earth);
+
 void sendFakeGPS(const geometry_msgs::Point& point)
 {
-    ROS_INFO("x: %f", point.x);
+    double lat, lon, h;
+    ROS_INFO("x: %f\ny: %f\nz: %f\n", point.x, point.y, point.z);
+    proj.Reverse(point.x, point.y, point.z, lat, lon, h);
+    ROS_INFO("latitude: %f\nlongitude: %f\naltitude: %f", lat, lon, h);
 }
 
 int main(int argc, char **argv)
