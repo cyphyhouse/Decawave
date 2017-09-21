0aea3ea8b483dc280d553b0c5b9b71cf491ffd9f
Joao Paulo Porto <jpjporto@gmail.com>
2017-09-21 16:49:09 -0500

    approximation so we dont have to use the geographiclib

--- ROS/cyphyhouse/quadcopter/src/fakegps.cpp
+++ ROS/cyphyhouse/quadcopter/src/fakegps.cpp
@@ -9,7 +9,7 @@
 #include "geometry_msgs/Point.h"
 #include "ros/package.h"
 
-static const double lat0 = 40 + 7/60.0, lon0 = -88 - 14/60.0; // CSL GPS coords
+static const double lat0 = 0, lon0 = 0;
 
 static GeographicLib::Geocentric earth(GeographicLib::Constants::WGS84_a(), GeographicLib::Constants::WGS84_f());
 static GeographicLib::LocalCartesian proj(lat0, lon0, 0, earth);
@@ -17,9 +17,15 @@ static GeographicLib::LocalCartesian proj(lat0, lon0, 0, earth);
 void sendFakeGPS(const geometry_msgs::Point& point)
 {
     double lat, lon, h;
-    ROS_INFO("x: %f\ny: %f\nz: %f\n", point.x, point.y, point.z);
+    ROS_INFO("x: %f, y: %f, z: %f\n", point.x, point.y, point.z);
     proj.Reverse(point.x, point.y, point.z, lat, lon, h);
-    ROS_INFO("latitude: %f\nlongitude: %f\naltitude: %f", lat, lon, h);
+    ROS_INFO("latitude: %f, longitude: %f, altitude: %f", lat, lon, h);
+    
+    double phi, theta;
+    double r = 6371000;
+    theta = 90-atan2(r,point.y)*180/3.14159265359;
+    phi = 90-acos(point.x/r)*180/3.14159265359;
+    ROS_INFO("Approx lat: %f, Approx lon: %f", theta, phi);
 }
 
 int main(int argc, char **argv)
