76ca2573f6385ed6cb791bdec8dc1820628ab8bb
Yhoas Olivas Hernandez <yholivas@gmail.com>
2017-10-05 17:26:16 -0500

    need to fix library linkage issues

--- ROS/cyphyhouse/quadcopter/src/fakegps.cpp
+++ ROS/cyphyhouse/quadcopter/src/fakegps.cpp
@@ -2,30 +2,39 @@
 #include <iostream>
 #include <cmath>
 
-#include <GeographicLib/Geocentric.hpp>
-#include <GeographicLib/LocalCartesian.hpp>
+// #include <GeographicLib/Geocentric.hpp>
+// #include <GeographicLib/LocalCartesian.hpp>
+
+#include <mavros/mavros_plugin.h>
+#include <mavros/setpoint_mixin.h>
+
+#include <mavconn/interface.h>
 
 #include "ros/ros.h"
 #include "geometry_msgs/Point.h"
 #include "ros/package.h"
 
+using namespace mavlink;
+
 static const double lat0 = 0, lon0 = 0;
 
-static GeographicLib::Geocentric earth(GeographicLib::Constants::WGS84_a(), GeographicLib::Constants::WGS84_f());
-static GeographicLib::LocalCartesian proj(lat0, lon0, 0, earth);
+// static GeographicLib::Geocentric earth(GeographicLib::Constants::WGS84_a(), GeographicLib::Constants::WGS84_f());
+// static GeographicLib::LocalCartesian proj(lat0, lon0, 0, earth);
 
 void sendFakeGPS(const geometry_msgs::Point& point)
 {
-    double lat, lon, h;
-    ROS_INFO("x: %f, y: %f, z: %f\n", point.x, point.y, point.z);
-    proj.Reverse(point.x, point.y, point.z, lat, lon, h);
-    ROS_INFO("latitude: %f, longitude: %f, altitude: %f", lat, lon, h);
+    // double lat, lon, h;
+    // ROS_INFO("x: %f, y: %f, z: %f\n", point.x, point.y, point.z);
+    // proj.Reverse(point.x, point.y, point.z, lat, lon, h);
+    // ROS_INFO("latitude: %f, longitude: %f, altitude: %f", lat, lon, h);
     
     double phi, theta;
     double r = 6371000;
     theta = 90-atan2(r,point.y)*180/3.14159265359;
     phi = 90-acos(point.x/r)*180/3.14159265359;
     ROS_INFO("Approx lat: %f, Approx lon: %f", theta, phi);
+
+    mavlink::common::msg::HIL_GPS fix {};
 }
 
 int main(int argc, char **argv)
