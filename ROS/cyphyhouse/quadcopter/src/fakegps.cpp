1227234c8af288c51fc7ea10325a3d074ccd21ef
Yhoas Olivas Hernandez <yholivas@gmail.com>
2017-10-09 17:37:08 +0000

    populated HIL_GPS message, need to send to ardupilot and test

--- ROS/cyphyhouse/quadcopter/src/fakegps.cpp
+++ ROS/cyphyhouse/quadcopter/src/fakegps.cpp
@@ -2,39 +2,66 @@
 #include <iostream>
 #include <cmath>
 
-// #include <GeographicLib/Geocentric.hpp>
-// #include <GeographicLib/LocalCartesian.hpp>
+#include <GeographicLib/Geocentric.hpp>
+#include <GeographicLib/LocalCartesian.hpp>
 
+#include <eigen_conversions/eigen_msg.h>
 #include <mavros/mavros_plugin.h>
 #include <mavros/setpoint_mixin.h>
-
-#include <mavconn/interface.h>
 
 #include "ros/ros.h"
 #include "geometry_msgs/Point.h"
 #include "ros/package.h"
 
-using namespace mavlink;
-
 static const double lat0 = 0, lon0 = 0;
+static double old_stamp = ros::Time(0.0).toSec();
+static Eigen::Vector3d old_ecef(0,0,0);
 
-// static GeographicLib::Geocentric earth(GeographicLib::Constants::WGS84_a(), GeographicLib::Constants::WGS84_f());
-// static GeographicLib::LocalCartesian proj(lat0, lon0, 0, earth);
+static GeographicLib::Geocentric earth(GeographicLib::Constants::WGS84_a(), GeographicLib::Constants::WGS84_f());
+static GeographicLib::LocalCartesian proj(lat0, lon0, 0, earth);
 
 void sendFakeGPS(const geometry_msgs::Point& point)
 {
-    // double lat, lon, h;
-    // ROS_INFO("x: %f, y: %f, z: %f\n", point.x, point.y, point.z);
-    // proj.Reverse(point.x, point.y, point.z, lat, lon, h);
-    // ROS_INFO("latitude: %f, longitude: %f, altitude: %f", lat, lon, h);
-    
-    double phi, theta;
-    double r = 6371000;
-    theta = 90-atan2(r,point.y)*180/3.14159265359;
-    phi = 90-acos(point.x/r)*180/3.14159265359;
-    ROS_INFO("Approx lat: %f, Approx lon: %f", theta, phi);
-
     mavlink::common::msg::HIL_GPS fix {};
+    ros::Time stamp = ros::Time::now();
+    Eigen::Vector3d current_ecef(point.x, point.y, point.z);
+
+    double lat, lon, h;
+    ROS_INFO("x: %f, y: %f, z: %f\n", point.x, point.y, point.z);
+    proj.Reverse(point.x, point.y, point.z, lat, lon, h);
+    ROS_INFO("latitude: %f, longitude: %f, altitude: %f", lat, lon, h);
+
+    // compute velocity (borrowed from mavros)
+    Eigen::Vector3d vel = ((old_ecef - current_ecef) / (stamp.toSec() - old_stamp)) * 1e2;
+
+    // compute course over ground (borrowed from mavros)
+    double cog;
+    if (vel.x() == 0 && vel.y() == 0) {
+        cog = 0;
+    }
+    else if (vel.x() >= 0 && vel.y() < 0) {
+        cog = M_PI * 5 / 2 - atan2(vel.x(), vel.y());
+    }
+    else {
+        cog = M_PI / 2 - atan2(vel.x(), vel.y());
+    }
+
+    fix.time_usec = stamp.toNSec() / 1000;
+    fix.lat = lat * 1e7;
+    fix.lon = lon * 1e7;
+    fix.alt = h * 1e3;
+    fix.vel = vel.block<2, 1>(0, 0).norm();
+    fix.vn = vel.x();
+    fix.ve = vel.y();
+    fix.vd = vel.z();
+    fix.cog = cog * 1e2;
+    fix.eph = 2.0 * 1e2;
+    fix.epv = 2.0 * 1e2;
+    fix.fix_type = 3;
+    fix.satellites_visible = 5;
+
+    old_stamp = stamp.toSec();
+    old_ecef = current_ecef;
 }
 
 int main(int argc, char **argv)
