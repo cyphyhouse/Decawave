9976e824a470bdbf64e03f14fe458e1a23edaac7
yholivas <yholivas@gmail.com>
2017-11-29 13:01:37 -0600

    takeoff and landing works

--- ROS/cyphyhouse/quadcopter/src/fakegps.cpp
+++ ROS/cyphyhouse/quadcopter/src/fakegps.cpp
@@ -9,21 +9,34 @@
 #include <mavros/mavros_plugin.h>
 #include <mavros/setpoint_mixin.h>
 #include <mavconn/interface.h>
-#include <mavros_msgs/WaypointPush.h>
+
+#include <mavros_msgs/CommandInt.h>
+#include <mavros_msgs/CommandTOL.h>
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/SetMode.h>
 
 #include "ros/ros.h"
 #include "geometry_msgs/PoseStamped.h"
 #include "geometry_msgs/TwistStamped.h"
+#include "geometry_msgs/PointStamped.h"
 #include "ros/package.h"
 
 static const double lat0 = 40.116, lon0 = -88.224;	// IRL GPS coords
+double current_lat, current_lon, current_h = 0;
+
 ros::Time old_stamp = ros::Time(0.0);
-ros::ServiceClient client;
+
+ros::ServiceClient cmd_client;
+ros::ServiceClient arming_client;
+ros::ServiceClient takeoff_client;
+ros::ServiceClient land_client;
+ros::ServiceClient mode_client;
+
 geometry_msgs::Vector3 current_vel;
-static GeographicLib::Geoid egm96_5("egm96-5", "", true, true);
 
 static mavconn::MAVConnInterface::Ptr ardupilot_link;
 
+static GeographicLib::Geoid egm96_5("egm96-5", "", true, true);
 static GeographicLib::Geocentric earth(GeographicLib::Constants::WGS84_a(), GeographicLib::Constants::WGS84_f());
 static GeographicLib::LocalCartesian proj(lat0, lon0, 0, earth);
 
@@ -44,9 +57,13 @@ void sendFakeGPS(const geometry_msgs::PoseStamped::ConstPtr& pose)
         return;
 
     double lat, lon, h;
-    ROS_INFO("x: %f, y: %f, z: %f\n", point.x, point.y, point.z);
+    // ROS_INFO("x: %f, y: %f, z: %f\n", point.x, point.y, point.z);
     proj.Reverse(point.y, -point.x, point.z, lat, lon, h);
-    ROS_INFO("latitude: %f, longitude: %f, altitude: %f", lat, lon, (h + GeographicLib::Geoid::ELLIPSOIDTOGEOID * egm96_5(lat, lon)));
+    // ROS_INFO("latitude: %f, longitude: %f, altitude: %f", lat, lon, h);
+
+    current_lat = lat;
+    current_lon = lon;
+    current_h = h;
 
     // compute course over ground (borrowed from mavros)
     double cog;
@@ -64,8 +81,7 @@ void sendFakeGPS(const geometry_msgs::PoseStamped::ConstPtr& pose)
     fix.time_usec = stamp.toNSec() / 1000;
     fix.lat = lat * 1e7;
     fix.lon = lon * 1e7;
-    fix.alt = (h + GeographicLib::Geoid::ELLIPSOIDTOGEOID * egm96_5(lat, lon)) * 1e3;
-    // fix.alt = h * 1e3;
+    fix.alt = h * 1e3;
     fix.vel = sqrt(current_vel.x * current_vel.x + current_vel.y * current_vel.y + current_vel.z * current_vel.z) * 100;
     fix.vn = -current_vel.x * 100;
     fix.ve = current_vel.y * 100;
@@ -80,30 +96,73 @@ void sendFakeGPS(const geometry_msgs::PoseStamped::ConstPtr& pose)
     ardupilot_link.get()->send_message_ignore_drop(fix);
 }
 
-void sendWP(const geometry_msgs::Point& point)
+void sendWP(const geometry_msgs::PointStamped& stamped_point)
 {
-    mavros_msgs::WaypointPush waypoint_msg {};
-    mavros_msgs::Waypoint waypoint {};
-    double lat, lon, h;
-    proj.Reverse(point.y, -point.x, point.z, lat, lon, h);
-    waypoint_msg.request.start_index = 0;
-    waypoint.frame = 0; // global frame, not sure if that important
-    waypoint.command = 16;  // enum MAV_CMD_NAV_WAYPOINT
-    waypoint.is_current = true;
-    waypoint.autocontinue = true;   // autocontinue to next waypoint
-    waypoint.param1 = 1;    // hold time in secs
-    waypoint.param2 = 0.5;  // acceptance radius in meters
-    waypoint.param3 = 0;    // pass through waypoint
-    waypoint.param4 = NAN;  // yaw angle (don't need it so NaN)
-    waypoint.x_lat = lat;
-    waypoint.y_long = lon;
-    waypoint.z_alt = h;
+    mavros_msgs::SetMode mode_msg;
+    mode_msg.request.base_mode = 216;
+    mode_msg.request.custom_mode = "";
+    mode_client.call(mode_msg);
+    geometry_msgs::Point point = stamped_point.point;
+    std::string stamp = stamped_point.header.frame_id;
+    if (stamp == "0")    // takeoff
+    {
+        mavros_msgs::CommandBool arming_msg;
+        mavros_msgs::CommandTOL takeoff_msg;
+        arming_msg.request.value = true;
+        if (arming_client.call(arming_msg))
+            ROS_INFO("arming success");
+        else
+            ROS_INFO("arming failed");
+        takeoff_msg.request.min_pitch = 0; //have no idea about this
+        takeoff_msg.request.yaw = 0;
+        takeoff_msg.request.latitude = current_lat; // This is either the first waypoint or whatever the initial position is
+        takeoff_msg.request.longitude = current_lon;
+        takeoff_msg.request.altitude = current_h + 1;
 
-    // populate waypoint table with waypoint message (it's a vector)
-    waypoint_msg.request.waypoints.push_back(waypoint);
+        if (takeoff_client.call(takeoff_msg))
+            ROS_INFO("takeoff success");
+        else
+            ROS_INFO("takeoff failed");
+    }
+    else if (stamp == "2")   // land
+    {
+        mavros_msgs::CommandBool arming_msg;
+        mavros_msgs::CommandTOL land_msg;
+        land_msg.request.min_pitch = 0;
+        land_msg.request.yaw = 0;
+        land_msg.request.latitude = current_lat;
+        land_msg.request.longitude = current_lon;
+        land_msg.request.altitude = 0;
+        if (land_client.call(land_msg))
+            ROS_INFO("landing success");
+        else
+            ROS_INFO("landing failed");
+        arming_msg.request.value = false;
+        if (arming_client.call(arming_msg))
+            ROS_INFO("disarming success");
+        else
+            ROS_INFO("disarming failed");
+    }
+    else
+    {
+        mavros_msgs::CommandInt cmd_msg {};
+        double lat, lon, h;
+        proj.Reverse(point.y, -point.x, point.z, lat, lon, h);
+        cmd_msg.request.frame = 0;   // not sure if this does anything
+        cmd_msg.request.command = 192;  // enum MAV_CMD_NAV_WAYPOINT
+        cmd_msg.request.current = 1;   // apparently doesn't matter
+        cmd_msg.request.autocontinue = 1;
+        cmd_msg.request.param1 = -1;    // hold time in secs
+        cmd_msg.request.param2 = 1;  // acceptance radius in meters
+        //cmd_msg.request.param3 = 0;    // pass through waypoint
+        cmd_msg.request.param4 = NAN;  // yaw angle (don't need it so NaN)
+        cmd_msg.request.x = lat * 1E7;
+        cmd_msg.request.y = lon * 1E7;
+        cmd_msg.request.z = h;
 
-    // send it using service
-    client.call(waypoint_msg);
+        // send it using service
+        cmd_client.call(cmd_msg);
+    }
 }
 
 int main(int argc, char **argv)
@@ -111,10 +170,17 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "fakeGPS");
     ardupilot_link = mavconn::MAVConnInterface::open_url("udp://127.0.0.1:14550@");
     ros::NodeHandle n;
-    client = n.serviceClient<mavros_msgs::WaypointPush>("/mavros/mission/push");
+
+    cmd_client = n.serviceClient<mavros_msgs::CommandInt>("/mavros/cmd/command_int");
+    arming_client = n.serviceClient<mavros_msgs::CommandBool>("/mavros/cmd/arming");
+    takeoff_client = n.serviceClient<mavros_msgs::CommandTOL>("/mavros/cmd/takeoff");
+    land_client = n.serviceClient<mavros_msgs::CommandTOL>("/mavros/cmd/land");
+    mode_client = n.serviceClient<mavros_msgs::SetMode>("/mavros/set_mode");
+
     ros::Subscriber vel_sub = n.subscribe("/vrpn_client_node/cyphyhousecopter/twist", 1, getVelocity);
     ros::Subscriber sub = n.subscribe("/vrpn_client_node/cyphyhousecopter/pose", 1, sendFakeGPS);
     ros::Subscriber waypoint = n.subscribe("/starl/waypoints", 1, sendWP);  // second parameter is num of buffered messages
+
     ros::spin();
     return 0;
 }
